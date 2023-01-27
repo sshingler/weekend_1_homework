9f0359055c39e82ea7dd19e71994af8583d8c374
SShingler <s.shingler@hotmail.co.uk>
2023-01-27 13:41:40 +0000

    weekend homework - commit 1

---
+++ src/pet_shop.py
@@ -0,0 +1,8 @@
+# WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(pet_shop_name):
+    return(pet_shop_name["name"]) 
+
+def get_total_cash(total_cash):
+    return(total_cash["admin"]["total_cash"])
+
