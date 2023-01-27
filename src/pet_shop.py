bb10aeb2470a52561e88bfdf58dc81af61677454
SShingler <s.shingler@hotmail.co.uk>
2023-01-27 14:23:20 +0000

    weekend homework - commit 2

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -6,3 +6,10 @@ def get_pet_shop_name(pet_shop_name):
 def get_total_cash(total_cash):
     return(total_cash["admin"]["total_cash"])
 
+def add_or_remove_cash(total_cash, cash_add):
+    total_cash["admin"]["total_cash"] += cash_add
+
+#function name? - add_or_remove_cash 
+# parameters? - two parameters, - one dictionary, one number
+# what does function need to do? - add number to existing dictionary entry
+# what does function need to return? - 
